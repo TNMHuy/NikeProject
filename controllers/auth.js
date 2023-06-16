ea128b16093e039eb79d9a8bf210cfd6db870702
TNMHuy <mhuy2898@gmail.com>
2023-06-16 21:59:29 +0700

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,38 @@
-const checkAuth = () => {
-    let local = localStorage.getItem("LOGIN");
-    let isLogin = window.location.pathname == '/login.html'
-    if(local){
-        if(isLogin){
-            // let userType = JSON.parse(localStorage.getItem("LOGIN")).user.userType;
-            // let isAdmin = window.location.pathname == '/index.html'
-            window.location.assign('/index.html'); 
+// const checkAuth = () => {
+//     let local = localStorage.getItem("LOGIN");
+//     let isLogin = window.location.pathname == '/login.html'
+//     if(local){
+//         if(isLogin){
+//             // let userType = JSON.parse(localStorage.getItem("LOGIN")).user.userType;
+//             // let isAdmin = window.location.pathname == '/index.html'
+//             window.location.assign('/index.html'); 
 
-            // if(userType=='admin'){
-            //     if(isAdmin){
-            //         window.location.assign('/admin/index.html');
-            //     }else{
-            //     }
-            //     return
-            // }
+//             // if(userType=='admin'){
+//             //     if(isAdmin){
+//             //         window.location.assign('/admin/index.html');
+//             //     }else{
+//             //     }
+//             //     return
+//             // }
             
-        }
-        return
-    }
-    !isLogin && window.location.assign('/login.html')
-} 
-checkAuth()
-const checkUser = () => {
-    let local = localStorage.getItem("LOGIN");
-    let isLogin = window.location.pathname == '/admin/index.html'
-    let userType = JSON.parse(localStorage.getItem("LOGIN"))?.user.userType;
-    // console.log(userType.user.userType);
-    if(local){
-        if(isLogin&&userType=='user'){
-            window.location.assign('/index.html')
-        }
-        return
-    }
+//         }
+//         return
+//     }
+//     !isLogin && window.location.assign('/login.html')
+// } 
+// checkAuth()
+// const checkUser = () => {
+//     let local = localStorage.getItem("LOGIN");
+//     let isLogin = window.location.pathname == '/admin/index.html'
+//     let userType = JSON.parse(localStorage.getItem("LOGIN"))?.user.userType;
+//     // console.log(userType.user.userType);
+//     if(local){
+//         if(isLogin&&userType=='user'){
+//             window.location.assign('/index.html')
+//         }
+//         return
+//     }
 
-    !isLogin&&userType=='admin'&&window.location.assign('/admin/index.html')
-}
-checkUser()
+//     !isLogin&&userType=='admin'&&window.location.assign('/admin/index.html')
+// }
+// checkUser()
